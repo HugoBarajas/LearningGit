77f8b35b9a9b7e8e29aa1503514185bcf6f47e9a
Victor Hugo Barajas Santibañez <comodindelarge@gmail.com>
2023-12-19 19:30:20 -0600

    quite los espacios de YouTubeViewController

--- ExamenIngenio2/YoutubeMVP/YouTubeViewController.swift
+++ ExamenIngenio2/YoutubeMVP/YouTubeViewController.swift
@@ -9,43 +9,41 @@ import UIKit
 import YouTubePlayer
 
 class YouTubeViewController: UIViewController {
-    
-    var spaceXInfo : ModelSpaceX?
+  
+  var spaceXInfo : ModelSpaceX?
   var viewModel : YouTubeViewModel!
-
-    var misionViewVideo: UIView = {
-      var view = UIView()
-      view.backgroundColor = .black
-      view.layer.borderWidth = 4
-      view.layer.borderColor = UIColor.white.cgColor
-      view.clipsToBounds = true
-        return view
-    }()
+  
+  var misionViewVideo: UIView = {
+    var view = UIView()
+    view.backgroundColor = .black
+    view.layer.borderWidth = 4
+    view.layer.borderColor = UIColor.white.cgColor
+    view.clipsToBounds = true
+    return view
+  }()
+  
+  var missionVideo : YouTubePlayerView = {
+    var video = YouTubePlayerView()
+    return video
     
-    var missionVideo : YouTubePlayerView = {
-      var video = YouTubePlayerView()
-      return video
-      
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      initInfo()
-      initUI()
-    }
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    initInfo()
+    initUI()
+  }
   func initInfo(){
     spaceXInfo = viewModel.detalleInfo
   }
   
-    func initUI(){
-        
-        view.addSubview(misionViewVideo)
-         misionViewVideo.addAnchorsAndCenter(centerX: true, centerY: true, width: width, height: 400, left: nil, top: nil, right: nil, bottom: nil)
-
+  func initUI(){
     
-        missionVideo.loadVideoID((spaceXInfo?.links?.youtube_id)!)
-        missionVideo.frame = CGRect(x: 0, y: 0, width: width, height: 400)
-        misionViewVideo.addSubview(missionVideo)
-    }
-
+    view.addSubview(misionViewVideo)
+    misionViewVideo.addAnchorsAndCenter(centerX: true, centerY: true, width: width, height: 400, left: nil, top: nil, right: nil, bottom: nil)
+    missionVideo.loadVideoID((spaceXInfo?.links?.youtube_id)!)
+    missionVideo.frame = CGRect(x: 0, y: 0, width: width, height: 400)
+    misionViewVideo.addSubview(missionVideo)
+  }
+  
 }
